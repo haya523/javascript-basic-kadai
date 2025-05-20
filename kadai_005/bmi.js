44c8bd50f19fd79ab05bebf0095d46633c7ef826
haya523 <a.1989052396@gmail.com>
2025-05-21 06:30:59 +0900

    kadai_005

---
+++ kadai_005/bmi.js
@@ -0,0 +1,13 @@
+//体重：68kg身長：1.7m 計算式：[体重(kg)]÷[身長(m)×身長(m)]
+
+//変数　体重
+let weight = 68;
+
+//変数　身長
+let height = 1.7;
+
+//定数　BMIの計算式
+const bmi = weight / (height * height);
+
+//コンソール出力
+console.log(bmi);
