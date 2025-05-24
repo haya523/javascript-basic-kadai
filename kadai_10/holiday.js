13a6f56beefed958dc9ad55fcdc8bf1011e8b5c5
haya523 <a.1989052396@gmail.com>
2025-05-24 18:58:59 +0900

    kadai_10

--- kadai_10/holiday.js
+++ kadai_10/holiday.js
@@ -1,13 +1,18 @@
 const holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"]
 
 // for文の場合
-for (let i = 0; i < 2; i++) {
- holidays.forEach(day => console.log(day));
+for (let j = 0; j < 2; j++) {
+  for (let i = 0; i < holidays.length; i++) {
+    console.log(holidays[i]);
+  }
 }
-
 // while文の場合
-let i = 0;
-while (i < 2) {
-  holidays.forEach(day => console.log(day));
-  i++;
+let j = 0;
+while (j < 2) { // 2周させる
+  let i = 0;
+  while (i < holidays.length) { // 配列を1周
+    console.log(holidays[i]);
+    i++;
+  }
+  j++;
 }
